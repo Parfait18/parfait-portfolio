44d1ca62760e0889a4407f0d68e71e0a1ee839f0
Parfait AHOUANTO <parfait.ahouanto@mehriso.com>
2023-11-14 18:12:05 +0100

    fix: fix particle bg config error

--- src/app/components/ParticleBg.tsx
+++ src/app/components/ParticleBg.tsx
@@ -3,7 +3,6 @@ import { useCallback } from "react";
 import type { Container, Engine } from "tsparticles-engine";
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
-import particlesConfigs  from '../configs/particles-configs';
 
 export default function ParticleBg () {
     const particlesInit = useCallback(async (engine: Engine) => {
@@ -23,7 +22,86 @@ export default function ParticleBg () {
             id="tsparticles"
             init={particlesInit}
             loaded={particlesLoaded}
-             options={particlesConfigs}
+             options={{
+                background: {
+                    color: {
+                        value: "transparent",
+                    },
+                },
+                fpsLimit: 120,
+                interactivity: {
+                    events: {
+                        onClick: {
+                            enable: true,
+                            mode: "repulse",
+                        },
+                        onHover: {
+                            enable: true,
+                            mode: "bubble",
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        push: {
+                            quantity: 4,
+                        },
+                        repulse: {
+                            distance: 200,
+                            duration: 0.4,
+                        },
+                    },
+                },
+                particles: {
+                    color: {
+                        value: "#778da9",
+                    },
+                    links: {
+                        color: "#778da9",
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.5,
+                        width: 1,
+                    },
+                    collisions: {
+                        enable: true,
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: true,
+                        speed: 6,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 800,
+                        },
+                        value: 80,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        random: true,
+                        value: {
+                            min: 1,
+                            max: 3,
+                        },
+                        animation: {
+                            speed: 4,
+                            minimumValue: 0.3,
+                        },
+                    },
+                },
+                detectRetina: true,
+            }}
         />
     );
 };
